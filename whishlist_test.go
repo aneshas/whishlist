16f75ec40b323629fec8cbc2de7b16b11e9e9828
Anes Hasicic <me@anes.io>
2022-05-31 11:43:08 +0200

    Updated the test

--- whishlist_test.go
+++ whishlist_test.go
@@ -1,12 +1,37 @@
-// a suite of acceptance level tests against your core (hexagon)
+// A suite of ACCEPTANCE LEVEL tests against your core (hexagon)
 package main
 
-import "testing"
+import (
+	"context"
+	"testing"
+)
 
+// This test is the first user of our system and also
+// the first driving adapter.
+// This is obviously an incomplete test but it does demark our hexagon boundary
 func TestShould_Add_An_Item_To_Whishlist(t *testing.T) {
-	// mock or fake your adapters
-	// construct use case
-	// act / assert
+	fakeSave := func(ctx context.Context, i Item) error {
+		return nil
+	}
+
+	fakeNotifiy := func(ctx context.Context, i Item) error {
+		return nil
+	}
+
+	addItem := NewAddItemToWhishlistFunc(fakeSave, fakeNotifiy)
+
+	addedItem, err := addItem(context.TODO(), ItemToAdd{
+		Name: "A fancy AMZN item",
+		Note: "We wants it",
+	})
+
+	if err != nil {
+		t.Fatalf("no error expected, got: %v", err)
+	}
+
+	if addedItem.ItemID == 0 {
+		t.Fatal("no id assigned")
+	}
 }
 
-// Other tests
+// Other acceptance level tests
